26dfb5035f947f37f33e91ab047724febcbd76ef
Barto <ponsbarto@gmail.com>
2024-01-18 10:12:26 +0100

    Completada el codigo de la migration CreateDesigns con tabla llamada designs

--- src/database/migrations/1705568266706-CreateDesigns.ts
+++ src/database/migrations/1705568266706-CreateDesigns.ts
@@ -1,11 +1,51 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateDesigns1705568266706 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+              name: "designs",
+              columns: [
+                {
+                  name: "id",
+                  type: "int",
+                  isPrimary: true,
+                  isGenerated: true,
+                  generationStrategy: "increment",
+                },
+                {
+                  name: "artist_id",
+                  type: "int",
+                  isUnique: true,
+                },
+                {
+                  name: "name",
+                  type: "varchar",
+                  length: "50",
+                },
+                {
+                  name: "image",
+                  type: "varchar",
+                  length: "50",
+                  isNullable: true,
+                },
+              ],
+              foreignKeys: [
+                {
+                  columnNames: ["artist_id"],
+                  referencedTableName: "artists",
+                  referencedColumnNames: ["id"],
+                  onDelete: "CASCADE",
+                },
+              ],
+            }),
+            true
+          );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("designs");
     }
 
 }
