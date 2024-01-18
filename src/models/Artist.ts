ab00c17951c6a0a8b4b1351e15d8f12e3ea29804
Barto <ponsbarto@gmail.com>
2024-01-18 12:43:54 +0100

    Completar model Artist con relaciones uno a uno con user uno a muchos con appointments y uno a muchos con design

--- src/models/Artist.ts
+++ src/models/Artist.ts
@@ -1,6 +1,35 @@
-import { Entity } from "typeorm"
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, JoinColumn, PrimaryColumn } from "typeorm"
+import { User } from "./User"
+import { Appointment } from "./Appointment"
+import { Design } from "./Design"
 
-@Entity()
-export class Artist {
+@Entity("artists")
+export class Artist extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+  
+    @Column()
+    first_name!: string;
+  
+    @Column()
+    last_name!: string;
+  
+    @Column()
+    phone_number!: string;
+  
+    @Column()
+    tattoo_style!: string;
+ 
+    @OneToOne(() => User, (user) => user.artist)
+    @JoinColumn({ name: "user_id" })
+    user!: User;
+  
+
+    @OneToMany(() => Appointment, (appointment) => appointment.artist)
+    appointment!: Appointment;
+  
+   
+    @OneToMany(() => Design, (design) => design.artist)
+    design!: Design;
 
 }
