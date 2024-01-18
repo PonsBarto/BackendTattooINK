be6adce75a1daff3641cfabd3159fc7a6c127916
Barto <ponsbarto@gmail.com>
2024-01-18 12:11:44 +0100

    Completar model Client con las relaciones uno a uno con users y uno a muchos con appointment

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,6 +1,29 @@
-import { Entity } from "typeorm"
+import { Entity, OneToMany, Column, OneToOne, PrimaryGeneratedColumn,JoinColumn, BaseEntity } from "typeorm"
+import { Role } from "./Role"
+import { User } from "./User"
+import { Appointment } from "./Appointment"
 
-@Entity()
-export class Client {
+
+@Entity("clients")
+export class Client extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    first_name!: string
+
+    @Column()
+    last_name!: string
+
+    @Column()
+    phone_number!: string
+
+    @OneToOne(() => User, (user) => user.client)
+    @JoinColumn({name: "user_id"})
+    user!: User;
+
+    @OneToMany(() => Appointment, (appointment) => appointment.client)
+    appointment!: Appointment;
 
 }
