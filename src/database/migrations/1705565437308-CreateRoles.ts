04e1221bdad85e9b02165b052c2e800f764611c4
Barto <ponsbarto@gmail.com>
2024-01-18 09:33:42 +0100

    Creada la tabla roles en la migracion CreateRoles

--- src/database/migrations/1705565437308-CreateRoles.ts
+++ src/database/migrations/1705565437308-CreateRoles.ts
@@ -1,11 +1,34 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateRoles1705565437308 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+               name: "roles",
+               columns: [
+                  {
+                     name: "id",
+                     type: "int",
+                     isPrimary: true,
+                     isGenerated: true,
+                     generationStrategy: "increment",
+                  },
+                  {
+                     name: "role_name",
+                     type: "varchar",
+                     length: "50",
+                     isUnique: true,
+                  },
+               ],
+            }),
+            true
+        );    
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("roles");
     }
 
 }
