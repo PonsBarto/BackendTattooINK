499d4d9a8510674e0a4d372a805cfd79d7f210a4
Barto <ponsbarto@gmail.com>
2024-01-18 12:47:15 +0100

    Completar model Appointment con relacion muchos a uno con clientes y muchos a uno con artist

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,6 +1,25 @@
-import { Entity } from "typeorm"
+import { Entity, BaseEntity, Column, ManyToOne,PrimaryGeneratedColumn, JoinColumn } from "typeorm"
+import { Client } from "./Client"
+import { Artist } from "./Artist"
 
-@Entity()
-export class Appointment {
+@Entity("appointments")
+export class Appointment extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    appointment_date!: Date
+
+    @Column()
+    shift!: string
+ 
+    @ManyToOne(() => Client, (client) => client.appointment)
+    @JoinColumn({name: "client_id"})
+    client!: Client;
+
+
+     @ManyToOne(() => Artist, (artist) => artist.appointment)
+     @JoinColumn ({name: "artist_id"})
+     artist!: Artist;
 
 }
