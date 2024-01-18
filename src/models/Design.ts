017c5a4a96641cc7b0e81b473b92cbc770b2d995
Barto <ponsbarto@gmail.com>
2024-01-18 12:05:40 +0100

    Completar model Design con un relacion muchos a uno con artist

--- src/models/Design.ts
+++ src/models/Design.ts
@@ -1,6 +1,20 @@
-import { Entity } from "typeorm"
+import { Entity, BaseEntity, Column, JoinColumn, PrimaryColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { Artist } from "./Artist"
 
-@Entity()
-export class Design {
+@Entity("designs")
+export class Design extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    design_number!: string
+
+    @Column()
+    image!: string
+
+    @ManyToOne (()=> Artist, (artist)=> artist.design)
+    @JoinColumn ({name: "artist_id"})
+    artist!: Artist;
 
 }
