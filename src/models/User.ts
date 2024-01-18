ece273bdd362b3a568824e5942cf88762f91b962
Barto <ponsbarto@gmail.com>
2024-01-18 11:50:38 +0100

    Completar model User, con relacion uno a uno con artistas, uno a uno con clientes y muchos a uno con role

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,30 @@
-import { Entity } from "typeorm"
+import { Entity, Column, BaseEntity, OneToOne, ManyToOne, PrimaryGeneratedColumn,JoinColumn } from "typeorm"
+import { Role } from "./Role"
+import { Client } from "./Client"
+import { Artist } from "./Artist"
 
-@Entity()
+@Entity("users")
 export class User {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    username!: string
+
+    @Column()
+    password_hash!: string
+
+    @Column()
+    email!: string
+
+    @ManyToOne(() => Role, (role) => role.user)
+    @JoinColumn ({name: "role"}) 
+    role!: Role;
+ 
+   @OneToOne(() => Client, (client) => client.user)
+   client?: Client;
+ 
+    @OneToOne(() => Artist, (artist) => artist.user)
+    artist?: Artist;
 
 }
