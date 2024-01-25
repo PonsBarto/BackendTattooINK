a88b63168216131d57be997a1828137ce10cd11e
Barto <ponsbarto@gmail.com>
2024-01-25 09:30:11 +0100

    Incluir nuevas routes relacionadas con el commit anterior

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { AuthController } from "../controllers/AuthController";
 
-// -----------------------------------------------------------------------------
+
 
 const router = express.Router();
 const authController = new AuthController();
 
 router.post("/registerClient", authController.registerClient);
 router.post("/registerArtist", authController.registerArtist);
+router.post("/login", authController.login);
+router.get("/user/:userId", authController.getUserProfile);
+router.put("/user/:userId", authController.updateUserProfile);
 
 export default router;
